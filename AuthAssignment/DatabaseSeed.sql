fe55f649512c00213a83226ca27adef6b98482a7
Jonas1q <maam0025@easv365.dk>
2024-04-11 12:48:57 +0200

    initial D

---
+++ AuthAssignment/DatabaseSeed.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE IF NOT EXISTS users (
+                                     user_id INTEGER PRIMARY KEY,
+                                     username TEXT UNIQUE NOT NULL,
+                                     user_role TEXT NOT NULL DEFAULT 'Registered',
+                                     password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS articles (
+                                        article_id INTEGER PRIMARY KEY,
+                                        title TEXT NOT NULL,
+                                        content TEXT NOT NULL,
+                                        author_id INTEGER NOT NULL,
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        FOREIGN KEY (author_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS comments (
+                                        comment_id INTEGER PRIMARY KEY,
+                                        content TEXT NOT NULL,
+                                        article_id INTEGER NOT NULL,
+                                        user_id INTEGER NOT NULL,
+                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        FOREIGN KEY (article_id) REFERENCES articles(article_id) ON DELETE CASCADE,
+                                        FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+UPDATE articles
+SET created_at = strftime('%Y-%m-%d %H:%M:%S', created_at);
+UPDATE comments
+SET created_at = strftime('%Y-%m-%d %H:%M:%S', created_at);
---
+++ AuthAssignment/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        RoleClaimType = ClaimTypes.Role,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+// Database context
+builder.Services.AddSingleton(new SQLiteDatabaseContext());
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
